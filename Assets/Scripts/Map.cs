e02c35d74cb4fc33f6de024dee44e63d4056ea14
Savlon <davie_sav27@hotmail.com>
2015-01-12 14:01:55 +1000

    Procedurally Generated Binary Map
    A map created procedurally that utilises numbers represented in binary
    format to set a tiles sprite based on its surrounding tiles.

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour 
+{
+	public int width;
+	public int height;
+	public Tile[,] map;
+
+	public Tile[] tilePrefabs;
+
+	void Start () 
+	{
+		tilePrefabs = Resources.LoadAll <Tile> ("Prefabs");
+		CreateMap ();
+	}
+
+	public void CreateMap ()
+	{
+		map = new Tile[width, height];
+		for (int y = 0; y < map.GetLength (1); y++)
+		{
+			for (int x = 0; x < map.GetLength (0); x++)
+			{
+				Tile tile = Instantiate (tilePrefabs[Random.Range (0, tilePrefabs.Length)], new Vector3 (x, y, 0), Quaternion.identity) as Tile;
+				tile.transform.parent = transform;
+				map[x, y] = tile;
+			}
+		}
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+			StartCoroutine (CheckTiles ());
+	}
+
+	public IEnumerator CheckTiles ()
+	{
+		for (int y = 0; y < map.GetLength (1); y++)
+		{
+			for (int x = 0; x < map.GetLength (0); x++)
+			{
+				Tile tile = GetTile (x, y);
+				bool[] neighbours = tile.GetNeighbouringTiles (this);
+
+				bool up = neighbours[(int)Direction.Up];
+				bool right = neighbours[(int)Direction.Right];
+				bool down = neighbours[(int)Direction.Down];
+				bool left = neighbours[(int)Direction.Left];
+
+				int index = (up ? 0x1 : 0) | (right ? 0x2 : 0) | (down ? 0x4 : 0) | (left ? 0x8 : 0);
+
+				tile.SetTileSprite (index);
+
+			}
+		}
+		yield return null;
+	}
+
+	public Tile GetTile (int x, int y)
+	{
+		if (x >= 0 && y >= 0 && x < width && y < height)
+			return map[x, y];
+		return null;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Place different tile types here
+public enum TileType
+{
+	Grass,
+	Dirt
+}
+
+//Direction for each neighbour check. Can be changed to utilize 8 directions instead of 4
+public enum Direction
+{
+	Up = 0,
+	Right = 1,
+	Down = 2,
+	Left = 3
+}
+
+public class Tile : MonoBehaviour 
+{
+	public TileType type;
+	public Sprite[] directionalSprites;
+	public bool[] neighbourTiles;
+
+	private SpriteRenderer spriteRenderer;
+
+	void Start () 
+	{
+		spriteRenderer = GetComponent <SpriteRenderer> ();
+	}
+
+	public void SetTileSprite (int index)
+	{
+		if (index >= 0 && index < directionalSprites.Length)
+			spriteRenderer.sprite = directionalSprites[index];
+		else
+			Debug.LogWarning ("Could NOT locate sprite at index: " + index);
+	}
+	
+	public bool[] GetNeighbouringTiles (Map map)
+	{
+		neighbourTiles = new bool[4];
+
+		Tile up = map.GetTile (XPosition, YPosition + 1);
+		Tile down = map.GetTile (XPosition, YPosition - 1);
+		Tile left = map.GetTile (XPosition - 1, YPosition);
+		Tile right = map.GetTile (XPosition + 1, YPosition);
+
+		neighbourTiles[(int)Direction.Up] = (up && up.type == type) ? true : false;
+		neighbourTiles[(int)Direction.Right] = (right && right.type == type) ? true : false;
+		neighbourTiles[(int)Direction.Down] = (down && down.type == type) ? true : false;
+		neighbourTiles[(int)Direction.Left] = (left && left.type == type) ? true : false;
+
+		if (neighbourTiles[(int)Direction.Up] || neighbourTiles[(int)Direction.Right] ||
+		    neighbourTiles[(int)Direction.Down] || neighbourTiles[(int)Direction.Left])
+			ConnectedToSameTileType = true;
+		else
+			ConnectedToSameTileType = false;
+
+		return neighbourTiles;
+	}
+
+	public int XPosition { get { return Mathf.RoundToInt (transform.position.x);} set {}}
+	public int YPosition { get { return Mathf.RoundToInt (transform.position.y);} set {}}
+	public bool ConnectedToSameTileType {get; set;}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
